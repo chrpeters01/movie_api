f7e21dd8617c63dfb121436cf198ed9bac091d30
chrpeters01 <chrisbpeters01@gmail.com>
2023-11-02 10:52:30 -0500

    Update server.js
    revised server.js code

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
 
 http.createServer((request, response) => {
-  let url = new URL(request.url, `http://${request.headers.host}`);
+  let addr = request.url,
+    q = new URL(addr, 'http://' + request.headers.host),
+    filePath = '';
 
-  let filePath = "";
-
-  if (url.pathname.includes("documentation")) {
-    filePath = __dirname + "/documentation.html";
-  } else {
-    filePath = "index.html";
-  }
-
-  fs.appendFile(
-    "log.txt",
-    `URL: ${request.url} \nTimestamp: ${new Date()} \n \n`,
-    (err) => {
-      if (err) {
-        console.log("append error");
-      } else {
-        console.log("msg appended");
-      }
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
     }
-  );
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
 
   fs.readFile(filePath, (err, data) => {
     if (err) {
       throw err;
     }
 
-    response.writeHead(200, {
-      'Content-Type': 'text/html'
-    });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(data);
-    response.end('Hello Node!\n');
+    response.end();
 
   });
-}).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+}).listen(8080);
+console.log('My test server is running on Port 8080.');
