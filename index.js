2d499f9801109e057d79101ec1fa85538ba40b25
chrpeters01 <chrisbpeters01@gmail.com>
2023-11-06 12:27:52 -0600

    updates
    file / index.js updates

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 const express = require('express');
+morgan = require('morgan');
+fs = require('fs');
+path = require('path');
+
 const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
+  flags: 'a'
+})
+
+let topMovies = [
+  {
+    title: 'Saving Private Ryan',
+    Director: 'Steven Spielberg'
+  },
+  {
+    title: 'Good Will Hunting',
+    Director: 'Gus Van Sant'
+  },
+  {
+    title: 'The Departed',
+    Director: 'Martin Scorsese'
+  },
+
+]
+
+app.use(morgan('combined', {
+  stream: accessLogStream
+}));
+
+app.use(express.static('public'));
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix!')
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+  console.log('This app is listening on port 8080.');
+});
