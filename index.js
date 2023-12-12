c2ada9fc11eba3e017dbd69791265fc8a76b55cd
chrpeters01 <chrisbpeters01@gmail.com>
2023-12-12 09:15:19 -0600

    Update index.js
    index.js format update

--- index.js
+++ index.js
@@ -43,98 +43,40 @@ app.get('/directors/:name', (req, res) => {
   res.send('Successful GET request returns data about a director (bio, birth year, death year) by name');
 });
 
-// 5. Get All Users
-app.get('/users', async (req, res) => {
-  await Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
+// 5. 
+app.post('/users', (req, res) => {
+  res.send('Allow new users to register');
 });
 
-// 6. Add New User
-app.post('/users', async (req, res) => {
-  await Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
+// 6.
+app.put('/users/:userId', (req, res) => {
+  res.send('Allow users to update their user info (username, password, email, date of birth)');
 });
 
-// 7. Return Specific User by Username
-app.get('/users/:userId', async (req, res) => {
-  await Users.findOne({ Username: req.params.Username })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-// 8. Update User's Info
-app.put('/users/:userId', async (req, res) => {
-  await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
-    {
-      Username: req.body.Username,
-      Password: req.body.Password,
-      Email: req.body.Email,
-      Birthdate: req.body.Birthdate
-    }
-  },
-  { new: true }) // This line makes sure that the updated document is returned
-  .then((updatedUser) => {
-    res.json(updatedUser);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  })
-
-});
-
-
-// 9. 
+// 7. 
 app.post('/users/:userId/favorites', (req, res) => {
   res.send('Allow users to add a movie to their list of favorites');
 });
 
-
-
-
-
-
-// 10. 
+// 8. 
 app.delete('/users/:userId/favorites/:movieId', (req, res) => {
   res.send('Allow users to remove a movie from their list of favorites');
 });
 
-// 11. 
+// 9. 
 app.delete('/users/:userId', (req, res) => {
   res.send('Allow existing users to deregister')
 });
+
+
+
+
+
+
+
+
+
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
